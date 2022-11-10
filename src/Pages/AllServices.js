686af9b48a4190174bbff78a07498e7d084b8479
ashiq77 <web3.0.ashiq@gmail.com>
2022-11-10 19:17:34 +0200

    service update

--- src/Pages/AllServices.js
+++ src/Pages/AllServices.js
@@ -9,7 +9,7 @@ const AllServices = () => {
   const allService = useLoaderData();
     return (
       <div className='w-9/12 mx-auto flex flex-col justify-center'>
-        <h2 className='text-5xl text-center'>services: {allService.length}</h2>
+        <h2 className='text-2xl border- border-b-zinc-200 my-5 text-center font-semibold'>Your Trusted dentist Services</h2>
         <div className='grid md:grid-cols-3 gap-4'>
           {
             allService.map(service => <ServiceCard key={service._id} service={service}></ServiceCard>)
